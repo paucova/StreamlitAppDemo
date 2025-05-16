bbefd8412b8fed084fbb24fc0ddacd709c13885d
pau <a01383351@tec.mx>
2025-05-16 08:00:17 -0600

    added files

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
+
+# Pre trained model
+model = MobileNetV2(weights="imagenet")
+
+# Ui - i got this from internet
+st.title("Image Classification with MobileNetV2")
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file).convert("RGB")
+    st.image(img, caption="Uploaded Image", use_column_width=True)
+
+    img = img.resize((224, 224))
+    image_array = image.img_to_array(img)
+    image_array = np.expand_dims(image_array, axis=0)
+    image_array = preprocess_input(image_array)
+
+    predictions = model.predict(image_array)
+    decoded_predictions = decode_predictions(predictions, top=3)[0]
+
+    st.subheader("Top Predictions:")
+    for i, (imagenet_id, label, score) in enumerate(decoded_predictions):
+        st.write(f"**{label}**: {score*100:.2f}%")
