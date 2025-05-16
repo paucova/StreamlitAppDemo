f10a19a64da264c80bec6fa0b25c49fe7974b858
pau <a01383351@tec.mx>
2025-05-16 12:06:51 -0600

    fix app

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_
 
 # Pre trained model
 model = load_model("my_model.keras")
+class_names = ['Avión', 'Automóvil', 'Pájaro', 'Gato', 'Ciervo',
+               'Perro', 'Rana', 'Caballo', 'Barco', 'Camión']
 
 # Ui - i got this from internet
 st.title("Image Classification with Saul's model")
@@ -16,15 +18,17 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert("RGB")
     st.image(img, caption="Uploaded Image", use_column_width=True)
-
-    img = img.resize((224, 224))
+    
+    img = img.resize((32, 32))
     image_array = image.img_to_array(img)
     image_array = np.expand_dims(image_array, axis=0)
     image_array = preprocess_input(image_array)
 
-    predictions = model.predict(image_array)
-    decoded_predictions = decode_predictions(predictions, top=3)[0]
+    prediction = model.predict(image_array)
 
+    top_indices = prediction[0].argsort()[-3:][::-1]
+    decoded_predictions = [(class_names[i], float(prediction[0][i])) for i in top_indices]
+    
     st.subheader("Top Predictions:")
     for i, (imagenet_id, label, score) in enumerate(decoded_predictions):
         st.write(f"**{label}**: {score*100:.2f}%")
